95457ec5f1929d0745fa0e7836b6b33546f4f97c
Ojidaemo <vit.martsinovich@gmail.com>
2023-08-18 10:37:48 +0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Vitali Martsinovich on 2023-08-18.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
